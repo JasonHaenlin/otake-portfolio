983538a3502087b0137e0d6c0d16a0953c2fe33f
Jason Haenlin <jasonjas5252@gmail.com>
2019-11-06 18:28:21 +0100

    add animation and update path to resume

--- src/app/home-page/home-header/home-header.component.ts
+++ src/app/home-page/home-header/home-header.component.ts
@@ -1,10 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-home-header',
   templateUrl: './home-header.component.html',
-  styleUrls: ['./home-header.component.scss']
+  styleUrls: ['./home-header.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(200)
+      ]),
+      transition('* => void', [
+        animate(200, style({ transform: 'translateX(100%)' }))
+      ])
+    ])
+  ]
 })
 export class HomeHeaderComponent implements OnInit {
   public targetElement = null;
